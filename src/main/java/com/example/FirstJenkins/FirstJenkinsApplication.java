a81b25ab41900fb18bb93345f7de49cfba364796
Dev-With-Shiv <128348282+Dev-With-Shiv@users.noreply.github.com>
2025-05-04 14:50:22 +0530

    Initial commit

---
+++ src/main/java/com/example/FirstJenkins/FirstJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJenkinsApplication.class, args);
+	}
+
+}
