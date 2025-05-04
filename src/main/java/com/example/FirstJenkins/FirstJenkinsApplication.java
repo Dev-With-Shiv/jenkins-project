119b5ce3d765a4aeda296281372e7540396167c8
Dev-With-Shiv <128348282+Dev-With-Shiv@users.noreply.github.com>
2025-05-04 14:55:27 +0530

    added first controller api

--- src/main/java/com/example/FirstJenkins/FirstJenkinsApplication.java
+++ src/main/java/com/example/FirstJenkins/FirstJenkinsApplication.java
@@ -2,12 +2,25 @@ package com.example.FirstJenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/jenkins")
 public class FirstJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstJenkinsApplication.class, args);
+
+
+	}
+
+	@GetMapping
+	public ResponseEntity<String> getJenkins(){
+		return ResponseEntity.ok("I am from Jenkins");
 	}
 
 }
